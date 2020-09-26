e542d66fdb492275a3e052b00b771c6a671ac8e4
Carsten König <Carsten.Koenig@hotmail.de>
2020-09-26 19:53:38 +0200

    draw grid cells

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,9 @@ fn main() {
         .unwrap();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
-            clear([0.5, 0.5, 0.5, 1.0], g);
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            draw_board(&c, g);
+
             draw::draw_block(colors::RED, 0, 19, &c, g);
             draw::draw_block(colors::GREEN, 1, 19, &c, g);
             draw::draw_block(colors::BLUE, 2, 19, &c, g);
@@ -29,5 +31,13 @@ fn main() {
             draw::draw_block(colors::YELLOW, 0, 0, &c, g);
             draw::draw_block(colors::ORANGE, 9, 0, &c, g);
         });
+    }
+}
+
+fn draw_board(c: &Context, g: &mut G2d) {
+    for y in 0..20 {
+        for x in 0..10 {
+            draw::draw_block([0.1, 0.1, 0.1, 1.0], x, y, &c, g);
+        }
     }
 }
