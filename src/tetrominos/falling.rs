3b7d69c83f79ce146800faf614e4baac853090a0
Carsten König <Carsten.Koenig@hotmail.de>
2020-09-27 18:17:24 +0200

    can move falling tetrominos

--- src/tetrominos/falling.rs
+++ src/tetrominos/falling.rs
@@ -14,6 +14,14 @@ impl FallingTetromino {
         self.coord += point(0, 1)
     }
 
+    pub fn move_left(&mut self) {
+        self.coord += point(-1, 0)
+    }
+
+    pub fn move_right(&mut self) {
+        self.coord += point(1, 0)
+    }
+
     pub fn draw(&self, c: &Context, g: &mut G2d) {
         self.tetromino.draw(self.coord, &c, g);
     }
